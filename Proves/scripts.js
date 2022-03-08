f80307b961ef78f7ce5ce483dc5ceda86a0d3968
JSansoP <61268003+JSansoP@users.noreply.github.com>
2022-03-08 18:47:53 +0100

    cositas
    si el video no os funciona es pq no esta en el repo, lo tengo en local :+1:

--- Proves/scripts.js
+++ Proves/scripts.js
@@ -1,6 +1,5 @@
 function init() {
-  var video = document.createElement("video");
-  video.setAttribute("id", "video");
+  var video = document.getElementById("content");
   if (video.canPlayType) {
     if (video.canPlayType("video/ogg")) {
       video.src = "http://techslides.com/demos/sample-videos/small.ogv";
@@ -10,40 +9,36 @@ function init() {
     }
     video.setAttribute("height","500px")
     //video.setAttribute("controls", "controls");
-    document.body.appendChild(video);
   } else {
-    var div = document.createElement("div");
+    var div = document.getElementById("video-container");
     div.innerHTML = "Video not supported by your browser.";
-    document.body.appendChild(div);
   }
 }
 
 function toggleVideo(){
-  var vid = document.getElementById("video"); 
+  var vid = document.getElementById("content"); 
   var but = document.getElementById("toggleButton");
   if(vid.paused){
-    console.log("a");
     but.innerText = "Pause";
     vid.play();
   } else{
-    console.log("b");
     but.innerText = "Play";
     vid.pause();
   }
 }
 
 function playVid() {
-  var vid = document.getElementById("video");
+  var vid = document.getElementById("content");
   vid.play();
 }
 
 function pauseVid() {
-  var vid = document.getElementById("video");
+  var vid = document.getElementById("content");
   vid.pause();
 }
 
 function forward5(){
-  var vid = document.getElementById("video");
+  var vid = document.getElementById("content");
   vid.currentTime = vid.currentTime + 5;
 }
 
