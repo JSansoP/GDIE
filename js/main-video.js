d4e2053de86f54720c5a37b5f6262d4fa10710af
Strunky25 <41077084+Strunky25@users.noreply.github.com>
2022-03-30 22:04:21 +0200

    avances reproductor

--- js/main-video.js
+++ js/main-video.js
@@ -5,24 +5,39 @@
 const video = document.querySelector(".video");
 const playButton = document.querySelector(".play");
 const playButtonIcon = playButton.querySelector("i");
-const stopButton = document.querySelector(".stop");
+const volumeIcon = document.querySelector("#volume-icon");
+const volumeSlider = document.querySelector(".volume")
 const progressBar = document.querySelector(".progress")
 const timestamp = document.querySelector(".timestamp")
+const fullButton = document.querySelector(".full");
 
 // Listen for events
 video.addEventListener("click", playPauseVideo);
 progressBar.addEventListener("change", setVideoProgress)
 playButton.addEventListener("click", playPauseVideo);
-stopButton.addEventListener("click", stopVideo);
+volumeSlider.addEventListener("change", changeVolume);
 video.addEventListener("timeupdate", updateVideoProgress)
+fullButton.addEventListener("click", fullScreen)
+
+// init texttrack
+for (var i = 0; i < video.textTracks.length; i++) {
+    if (video.textTracks[i].label == "kda") {
+        kda = video.textTracks[i]
+        console.log(video.textTracks[i].cues)
+    }
+}
+for (var i = 0; i < kda.cues.length; i++) {
+    console.log(kda.cues[i].startTime)
+}
+
 
 // Utility functions
 function playPauseVideo() {
-//   if (video.paused) {
-//     video.play();
-//   } else {
-//     video.pause();
-//   }
+    //   if (video.paused) {
+    //     video.play();
+    //   } else {
+    //     video.pause();
+    //   }
     video[video.paused ? "play" : "pause"]()
     playButtonToggleIcon()
 }
@@ -50,7 +65,7 @@ function setVideoProgress() {
 
 function updateVideoProgress() {
     progressBar.value = Number((video.currentTime / video.duration) * 100)
-    let minutes = Math.floor(video.currentTime / 60)    
+    let minutes = Math.floor(video.currentTime / 60)
     let seconds = Math.floor(video.currentTime % 60)
     if (minutes < 10) {
         minutes = "0" + minutes
@@ -60,3 +75,34 @@ function updateVideoProgress() {
     }
     timestamp.textContent = `${minutes}:${seconds}`
 }
+
+function changeVolume() {
+    console.log(volumeIcon)
+    if (volumeSlider.value == 0) {
+        volumeIcon.classList.remove("fa-volume-up")
+        volumeIcon.classList.remove("fa-volume-down")
+        volumeIcon.classList.add("fa-volume-mute")
+    } else if (volumeSlider.value < 50) {
+        volumeIcon.classList.remove("fa-volume-up")
+        volumeIcon.classList.remove("fa-volume-mute")
+        volumeIcon.classList.add("fa-volume-down")
+    } else {
+        volumeIcon.classList.remove("fa-volume-mute")
+        volumeIcon.classList.remove("fa-volume-down")
+        volumeIcon.classList.add("fa-volume-up")
+    }
+    video.volume = volumeSlider.value / 100;
+}
+
+function fullScreen() {
+    if (video.requestFullscreen) { // Principal
+        video.requestFullscreen();
+    } else if (video.mozRequestFullScreen) { // Segun Navegador
+        video.mozRequestFullScreen();
+    } else if (video.webkitRequestFullscreen) {
+        video.webkitRequestFullscreen();
+    } else if (video.msRequestFullscreen) {
+        video.msRequestFullscreen();
+    }
+}
+
