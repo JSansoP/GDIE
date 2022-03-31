7d0a02eb27945744b86d2306fc0e312e77e18728
Strunky25 <41077084+Strunky25@users.noreply.github.com>
2022-03-31 18:26:54 +0200

    tutoria

--- js/video.js
+++ js/video.js
@@ -4,7 +4,9 @@
 // Grab DOM elements
 const video = document.querySelector(".video");
 const playButton = document.querySelector("#play");
-const playIcon = playButton.querySelector("i");
+const playIcon = document.querySelector("#play-icon");
+const forwardButton = document.querySelector("#forward");
+const muteButton = document.querySelector("#mute");
 const volumeIcon = document.querySelector("#volume-icon");
 const volumeSlider = document.querySelector("#volume-slider")
 const progressBar = document.querySelector("#progress-bar")
@@ -13,26 +15,34 @@ const fullButton = document.querySelector("#full-screen");
 
 // main
 addListeners();
+console.log(video.textTracks[0])
 
 // add listeners function
 function addListeners() {
-    video.addEventListener("click", playPauseVideo);
-    progressBar.addEventListener("change", setVideoProgress)
-    playButton.addEventListener("click", playPauseVideo);
-    volumeSlider.addEventListener("mousemove", changeVolume);
     video.addEventListener("timeupdate", updateVideoProgress)
+    video.addEventListener("click", playPauseVideo);
+    playButton.addEventListener("click", playPauseVideo);
+    forwardButton.addEventListener("click", forwardVideo);
+    muteButton.addEventListener("click", muteVideoAudio);
+    volumeSlider.addEventListener("mousemove", changeVolume);
+    progressBar.addEventListener("change", setVideoProgress)
     fullButton.addEventListener("click", fullScreen)
 }
 
-
-// print all texttrack cues
-function printCues() {
-    for (var i = 0; i < video.cues.length; i++) {
-        console.log(video.cues[i].text)
+// Utility functions
+function updateVideoProgress() {
+    progressBar.value = Number((video.currentTime / video.duration) * 100)
+    let minutes = Math.floor(video.currentTime / 60)
+    let seconds = Math.floor(video.currentTime % 60)
+    if (minutes < 10) {
+        minutes = "0" + minutes
     }
+    if (seconds < 10) {
+        seconds = "0" + seconds
+    }
+    timestamp.textContent = `${minutes}:${seconds}`
 }
 
-// Utility functions
 function playPauseVideo() {
     video[video.paused ? "play" : "pause"]()
     playButtonToggleIcon()
@@ -48,46 +58,48 @@ function playButtonToggleIcon() {
     }
 }
 
-function stopVideo() {
-    video.pause()
-    video.currentTime = 0
-    playButtonToggleIcon()
-    progressBar.value = 0
+function forwardVideo() {
+    console.log("forward")
+    video.currentTime += 10
+}
+
+function muteVideoAudio() {
+    console.log("mute")
+    video.muted = !video.muted
+    toggleVolumeIcon()
+}
+
+function toggleVolumeIcon() {
+    if (video.muted) {
+        volumeIcon.classList.remove("fa-volume-down")
+        volumeIcon.classList.remove("fa-volume-up")
+        volumeIcon.classList.add("fa-volume-mute")
+        volumeIcon.classList.remove("fa-volume-off")
+    } else if (video.volume == 0) {
+        volumeIcon.classList.remove("fa-volume-mute")
+        volumeIcon.classList.remove("fa-volume-down")
+        volumeIcon.classList.remove("fa-volume-up")
+        volumeIcon.classList.add("fa-volume-off")
+    } else if (video.volume < 0.5) {
+        volumeIcon.classList.add("fa-volume-down")
+        volumeIcon.classList.remove("fa-volume-up")
+        volumeIcon.classList.remove("fa-volume-mute")
+        volumeIcon.classList.remove("fa-volume-off")
+    } else {
+        volumeIcon.classList.remove("fa-volume-down")
+        volumeIcon.classList.add("fa-volume-up")
+        volumeIcon.classList.remove("fa-volume-mute")
+        volumeIcon.classList.remove("fa-volume-off")
+    }
+}
+
+function changeVolume() {
+    video.volume = volumeSlider.value;
+    toggleVolumeIcon()
 }
 
 function setVideoProgress() {
     video.currentTime = Number((progressBar.value * video.duration) / 100)
-}
-
-function updateVideoProgress() {
-    progressBar.value = Number((video.currentTime / video.duration) * 100)
-    let minutes = Math.floor(video.currentTime / 60)
-    let seconds = Math.floor(video.currentTime % 60)
-    if (minutes < 10) {
-        minutes = "0" + minutes
-    }
-    if (seconds < 10) {
-        seconds = "0" + seconds
-    }
-    timestamp.textContent = `${minutes}:${seconds}`
-}
-
-function changeVolume() {
-    console.log(volumeSlider.value)
-    if (volumeSlider.value == 0) {
-        volumeIcon.classList.remove("fa-volume-up")
-        volumeIcon.classList.remove("fa-volume-down")
-        volumeIcon.classList.add("fa-volume-mute")
-    } else if (volumeSlider.value < 50) {
-        volumeIcon.classList.remove("fa-volume-up")
-        volumeIcon.classList.remove("fa-volume-mute")
-        volumeIcon.classList.add("fa-volume-down")
-    } else {
-        volumeIcon.classList.remove("fa-volume-mute")
-        volumeIcon.classList.remove("fa-volume-down")
-        volumeIcon.classList.add("fa-volume-up")
-    }
-    video.volume = volumeSlider.value;
 }
 
 function fullScreen() {
